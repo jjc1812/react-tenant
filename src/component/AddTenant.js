662d0566f1a38e1368a22c9f8380edea47ed4c6c
jjc1812 <josejcronel98@gmail.com>
2022-01-19 20:57:42 -0300

    input data in form

--- src/component/AddTenant.js
+++ src/component/AddTenant.js
@@ -2,23 +2,27 @@ import React from 'react';
 
 export default () => {
 
+    const [name, setName] = React.useState("")
+    const [paymentStatus, setPaymentStatus] = React.useState("")
+    const [leaseEndDate, setLeaseEndDate] = React.useState("")
+
     return(
         <div className="container">
           <form>
             <div className="form-group">
               <label>Name</label>
-              <input className="form-control"/>
+              <input onChange={(e) => setName(e.target.value)} value={name} className="form-control"/>
             </div>
             <div className="form-group">
               <label>Payment Status</label>
-              <select className="form-control">
+              <select className="form-control" onClick={(e) => setPaymentStatus(e.target.value)}>
                 <option>CURRENT</option>
                 <option>LATE</option>
               </select>
             </div>
             <div className="form-group">
               <label>Lease End Date</label>
-              <input className="form-control"/>
+              <input onChange={(e) => setLeaseEndDate(e.target.value)} value={leaseEndDate} className="form-control"/>
             </div>
             <button className="btn btn-primary">Save</button>
             <button className="btn">Cancel</button>
